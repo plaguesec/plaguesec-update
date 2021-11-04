5580207c10633d71607d5d9cee0f2fe0b0f23f63
run3 <run3@plaguesec.com>
2021-11-04 17:48:17 +0800

    Add KDE and XFCE update functions

--- update.sh
+++ update.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+update_xfce(){
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+}
+
+update_kde(){
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+}
+
+if [ $XDG_CURRENT_DESKTOP == "XFCE" ]
+    then
+        update_xfce
+elif [ $XDG_CURRENT_DESKTOP == "KDE" ]
+    then
+        update_kde
+else
+    echo "The desktop environment is not Supported"
+fi
