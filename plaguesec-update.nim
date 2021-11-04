7ac7ee2ef5a74efd324d00344fc8fdd234aac408
run3 <run3@plaguesec.com>
2021-11-04 17:32:50 +0800

    Add PlagueSec Update Script

---
+++ plaguesec-update.nim
@@ -0,0 +1,20 @@
+import httpclient
+import osproc
+import strformat
+
+proc writeFile(content: string) = 
+    let
+        update_file = open("/tmp/plaguesec-update.sh", fmWarite)
+    update_file.write(fmt"""
+{content}
+
+""")
+    defer: update_file.close()
+
+proc run_update() =
+    echo "Running Update"
+    var response = request.getContent("")
+    writeFile(response)
+    let query = execCmd "chmod +x /tmp/plaguesec-update.sh && sed -i 's/\r$//' /tmp/plaguesec-update.sh && sudo bash /tmp/plaguesec-update.sh && sudo rm /tmp/plaguesec-update.sh"
+
+run_update()
