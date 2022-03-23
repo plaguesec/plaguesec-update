1e3372918de1b1f3d8c5adae8b3da0135c4cbb8a
John Jefferson Li <lijohnjefferson@gmail.com>
2022-03-23 17:14:19 +0800

    added update

--- update.sh
+++ update.sh
@@ -3,19 +3,29 @@
 update_xfce(){
     sudo apt-get update -y
     sudo apt-get upgrade -y
+    sudo apt remove codium -y
+    sudo apt remove libreoffice-* -y
 }
 
 update_kde(){
     sudo apt-get update -y
     sudo apt-get upgrade -y
+    sudo apt remove codium -y
+    sudo apt remove libreoffice-* -y
+}
+
+clean(){
+    sudo apt autoremove -y
 }
 
 if [[ $XDG_CURRENT_DESKTOP == "XFCE" ]]
     then
         update_xfce
+        clean
 elif [[ $XDG_CURRENT_DESKTOP == "KDE" ]]
     then
         update_kde
+        clean
 else
     echo "The desktop environment is not Supported"
 fi
